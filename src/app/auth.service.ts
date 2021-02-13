70cdbb734aadea09871c21d0b3f4a5019014f050
manthony <41161951+manthonyg@users.noreply.github.com>
2021-02-13 13:46:58 -0500

    faked an auth that can be changed from within the header component

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,5 +1,8 @@
+import { EventEmitter } from "@angular/core";
+
 export class AuthService {
   private _loggedIn: boolean = false;
+  public logStatusChange = new EventEmitter<boolean>();
 
   private _setUserLogStatus(logStatus) {
     return this._loggedIn = logStatus
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, OnDestroy, EventEmitter, Output } from '@angular/core';
 import { NavigationService } from "../services/navigation/navigation.service";
 import { RouterComponent } from "../router/router/router.component";
 import { Router } from "@angular/router";
+import { AuthService } from "../auth.service";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
 
   public selectedNavMenu: string = '';
-
-  constructor(private navigationService: NavigationService, private router: Router) { }
+  public loggedIn: boolean = false;
+  
+  constructor(private navigationService: NavigationService, private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
     this.navigationService.toggleDropdownMenu
@@ -19,10 +21,28 @@ export class HeaderComponent implements OnInit {
       return this.selectedNavMenu = navMenu
     })
     // this.router.navigate(['recipe-list', '2'])
-    
-
+    this.loggedIn = this.authService.getUserLogStatus()
+    // now subscribe to the observable event in case it is changed from within the component
+    this.authService.logStatusChange.subscribe((logStatus: boolean) => {
+      console.log('somethin changed')
+      this.loggedIn = logStatus
+    })
   }
-  
+
+  ngOnDestroy(): void {
+    this.authService.logStatusChange.unsubscribe()
+  }
+
+  login(): void {
+    this.authService.login()
+    this.authService.logStatusChange.emit(true)
+  }
+
+  logout(): void {
+    this.authService.logout()
+    this.authService.logStatusChange.emit(false)
+  }
+
   selectNavItem(navItem: string): void {
     this.navigationService.selectNavItemEvent.emit(navItem)
     console.log(navItem)
